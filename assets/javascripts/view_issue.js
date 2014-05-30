ce08ea1df078b37440a6261ea6871e85f718d906
Marius Balteanu <marius.balteanu@zitec.ro>
2014-05-30 15:36:02 +0300

    added missing js

--- assets/javascripts/view_issue.js
+++ assets/javascripts/view_issue.js
@@ -5,3 +5,39 @@ $(document).ready(function(){
     }
   });
 });
+
+
+function fix_buttons_panel () {
+  if (!(/\/issues\/\d+/).test($(location).attr('pathname'))) { return; }
+  var pnl = $('.js-issue-header');
+  if (pnl.length == 0) { return; }
+  var btn_form = $('#button_action_form');
+  var menu = $('#top-menu');
+  var menu_height = (menu.css('position') == 'fixed') ? menu.height : 0
+  var offset = pnl.offset().top - menu_height;
+
+
+  $(window).on('scroll resize', function () {
+    set_buttons_panel_location(pnl, offset, menu_height, btn_form);
+  }).trigger("scroll");
+
+  $(document.body).on('click', '.cancel_from_button', function () {
+    set_buttons_panel_location(pnl, offset, menu_height, btn_form);
+    return false;
+  });
+
+  $(document.body).on('click', '.lu_button', function () {
+    $('html,body').animate({ scrollTop: 0 }, 100);
+  });
+}
+function set_buttons_panel_location (panel, offset, menu_height, btn_form) {
+  if ($(window).scrollTop() - offset > 0 && !btn_form.is(':visible')) {
+    content = $('#content');
+    nxt = panel.next('.issue');
+    panel.addClass('lb_fixed').css({ left: content.offset().left, top: menu_height, width: content.outerWidth() - 6, paddingTop: 5, paddingLeft: 3, paddingRight: 3 });
+    nxt.css({ marginTop: panel.height() });
+  } else {
+    panel.removeClass("lb_fixed").css({ top: 'auto', width: 'auto', paddingTop: 0, paddingLeft: 0, paddingRight: 0 });
+    panel.next().css({ marginTop: 0 });
+  }
+}
