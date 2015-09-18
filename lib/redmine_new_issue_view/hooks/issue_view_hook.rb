930680044d2de22972dabd00a151d1a7ddefb972
Marius Balteanu <marius.balteanu@zitec.ro>
2015-09-18 12:18:48 +0300

    removed some unused files

--- lib/redmine_new_issue_view/hooks/issue_view_hook.rb
+++
@@ -1,6 +0,0 @@
-class IssueViewHook < Redmine::Hook::ViewListener
-  def view_layouts_base_html_head(context)
-    controller = context[:controller]
-    controller.render_to_string partial: 'new_issue_view/header_assets'
-  end
-end
