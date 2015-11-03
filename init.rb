84882261078d60880c9c53d929b82a15f4e34733
Marius Balteanu <marius.balteanu@zitec.ro>
2015-11-03 11:54:54 +0200

    bumped up version

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_new_issue_view do
   name 'New Issue View'
   author 'Zitec'
   description 'Overrides for redmine templates and methods'
-  version '1.1.0'
+  version '3.1.0'
   url 'https://github.com/sdwolf/redmine_new_issue_view'
   author_url 'http://www.zitec.com'
   settings partial: 'new_issue_view/plugin_settings'
