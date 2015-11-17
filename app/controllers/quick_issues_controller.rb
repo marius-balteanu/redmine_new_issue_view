2d2ff7ffe27fee7796d330a6c9d1ce393af075fa
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-11-17 18:12:15 +0200

    Refactored controller

--- app/controllers/quick_issues_controller.rb
+++ app/controllers/quick_issues_controller.rb
@@ -1,30 +1,53 @@
 class QuickIssuesController < ApplicationController
+  before_action :find_parent
+  before_action :find_tracker
+  before_action :find_assignee
+
   def add
-    @parent = Issue.find(params[:parent])
-    data = params[:data]
-    trackers = Tracker.where("name LIKE ?", "#{data[:tracker]}%").to_a
-    if trackers.size == 0
-      render json: { error: 'No matching tracker found' }, status: 400
-      return
-    elsif trackers.size > 1
-      render json: { error: 'More than one tracker found' }, status: 400
-      return
-    else
-      @tracker = trackers.first
-    end
-    @assignee = User.where(login: data[:assignee]).first
-    unless @assignee
-      render json: { error: 'Assignee not found' }, status: 400
-      return
-    end
-    @issue = Issue.new parent_id: @parent.id, subject: data[:subject],
-      tracker_id: @tracker.id, project_id: @parent.project_id,
-      author_id: User.current.id, estimated_hours: data[:estimation],
-      assigned_to_id: @assignee.id
+    @issue = Issue.new(
+      assigned_to_id:  @assignee.id,
+      author_id:       User.current.id,
+      estimated_hours: data[:estimation],
+      parent_id:       @parent.id,
+      project_id:      @parent.project_id,
+      subject:         data[:subject],
+      tracker_id:      @tracker.id
+    )
     if @issue.save
       render json: { success: 'Success!' }
     else
       render json: { error: @issue.errors.full_messages }, status: 400
+    end
+  end
+
+  private
+
+  def data
+    @data ||= params[:data]
+  end
+
+  def find_parent
+    @parent = Issue.where(id: params[:parent]).first
+    unless @assignee
+      render json: { error: 'Parent issue not found' }, status: 400
+    end
+  end
+
+  def find_tracker
+    trackers = Tracker.where("name LIKE ?", "#{data[:tracker]}%").to_a
+    if trackers.size == 0
+      render json: { error: 'No matching tracker found' }, status: 400
+    elsif trackers.size > 1
+      render json: { error: 'More than one tracker found' }, status: 400
+    else
+      @tracker = trackers.first
+    end
+  end
+
+  def find_assignee
+    @assignee = User.where(login: data[:assignee]).first
+    unless @assignee
+      render json: { error: 'Assignee not found' }, status: 400
     end
   end
 end
