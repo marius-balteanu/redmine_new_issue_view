d9b905c00fa1d6293c44703184d74b3fa2a2ad33
Marius BALTEANU <marius.balteanu@zitec.com>
2017-04-05 06:28:28 +0000

    show table header for subtasks and fix the class names

--- lib/redmine_new_issue_view/patches/issues_helper_patch.rb
+++ lib/redmine_new_issue_view/patches/issues_helper_patch.rb
@@ -9,6 +9,15 @@ module RedmineNewIssueView
           # It ads the thead section and changes the listed columns
           def render_descendants_tree(issue)
             s = '<table class="list issues odd-even">'
+            s << "<thead>
+                <tr>
+                  <th>#{ t 'views.table_headers.subject' }</th>
+                  <th>#{ t 'views.table_headers.status' }</th>
+                  <th>#{ t 'views.table_headers.assignee' }</th>
+                  <th>#{ t 'views.table_headers.estimated' }</th>
+                  <th>#{ t 'views.table_headers.spent' }</th>
+                </tr>
+              </thead>"
             issue_list(issue.descendants.visible.preload(:status, :priority, :tracker, :assigned_to).sort_by(&:lft)) do |child, level|
               css = "issue issue-#{child.id} hascontextmenu #{child.css_classes}"
               css << " idnt idnt-#{level}" if level > 0
@@ -18,8 +27,8 @@ module RedmineNewIssueView
                      content_tag('td', h(child.status), :class => 'status') +
                      content_tag('td', link_to_user(child.assigned_to), :class => 'assigned_to') +
                      # These columns were added
-                     content_tag('td', child.disabled_core_fields.include?('estimated_hours') ? "" : child.total_estimated_hours, class: "estimated-hours") +
-                     content_tag('td', (User.current.allowed_to?(:view_time_entries, @project) && child.total_spent_hours > 0) ? child.total_spent_hours : '', class: "spent-time"),
+                     content_tag('td', child.disabled_core_fields.include?('estimated_hours') ? '' : child.total_estimated_hours, class: "estimated_hours") +
+                     content_tag('td', (User.current.allowed_to?(:view_time_entries, @project) && child.total_spent_hours > 0) ? child.total_spent_hours : '', class: "spent_time"),
                      # This column was removed
                     #  content_tag('td', child.disabled_core_fields.include?('done_ratio') ? '' : progress_bar(child.done_ratio), :class=> 'done_ratio'),
                      :class => css)
